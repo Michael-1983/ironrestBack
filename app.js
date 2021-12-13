7e7592e9ba4d710bd488cb965e6aa6294e2456be
jfilinho <47835154+jfilinho@users.noreply.github.com>
2021-12-13 15:17:21 -0300

    codigo atualizado back

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(morgan("dev"));
 // Não esquecer de criar variável de ambiente com o endereço do seu app React (local ou deployado no Netlify)
 app.use(cors({ origin: process.env.REACT_APP_URL }));
 // rota do usuario 
-const userRoute  = require("./routes/CadastroVitma");
+const userRoute  = require("./routes/VitmaRouter");
 app.use("/api", userRoute);
 
 const familiaRoute  = require("./routes/familias.routes");
